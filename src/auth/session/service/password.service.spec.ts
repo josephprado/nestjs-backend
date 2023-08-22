19d98c2edb342380ca8a5a51c768d5532647bcb2
prado <pradodje@gmail.com>
2023-08-22 01:37:58 -0700

    Add password service tests

---
+++ src/auth/session/service/password.service.spec.ts
@@ -0,0 +1,144 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { Repository, UpdateResult } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { PasswordService } from './password.service';
+import { Password } from '../entity/password.entity';
+import { User } from 'src/auth/user/entity/user.entity';
+import { randomUUID } from 'crypto';
+import argon2 from 'argon2';
+import { NotFoundException } from '@nestjs/common';
+
+describe('PasswordService', () => {
+  let svc: PasswordService;
+  let repo: Repository<Password>;
+  let user: User;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        PasswordService,
+        {
+          provide: getRepositoryToken(Password),
+          useValue: {
+            save: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn()
+          }
+        }
+      ]
+    }).compile();
+
+    svc = module.get<PasswordService>(PasswordService);
+    repo = module.get<Repository<Password>>(getRepositoryToken(Password));
+    user = {
+      id: randomUUID(),
+      username: 'username',
+      email: 'username@email.com',
+      createDate: new Date(),
+      updateDate: new Date()
+    };
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  describe('create', () => {
+    it('should hash the password before saving to the database and attach the given user', async () => {
+      const raw = 'xyz';
+
+      const password = new Password();
+      password.user = user;
+      password.hash = expect.not.stringMatching(raw);
+
+      await svc.create(user, raw);
+      expect(repo.save).toBeCalledWith(password);
+    });
+
+    it('should return true when password is created successfully', async () => {
+      const raw = 'xyz';
+
+      const password = new Password();
+      password.user = user;
+      password.hash = expect.not.stringMatching(raw);
+
+      const actual = await svc.create(user, raw);
+      expect(actual).toEqual(true);
+    });
+  });
+
+  describe('update', () => {
+    it('should hash the password before saving to the database', async () => {
+      const raw = 'xyz';
+
+      jest
+        .spyOn(repo, 'update')
+        .mockResolvedValue({ affected: 1 } as UpdateResult);
+
+      await svc.update(user.id, raw);
+      expect(repo.update).toBeCalledWith(
+        { userId: user.id },
+        { hash: expect.not.stringMatching(raw) }
+      );
+    });
+
+    it('should return the number of affected rows', async () => {
+      const raw = 'xyz';
+      const affected = 1;
+
+      jest
+        .spyOn(repo, 'update')
+        .mockResolvedValue({ affected } as UpdateResult);
+
+      const actual = await svc.update(user.id, raw);
+      expect(actual).toEqual(affected);
+    });
+  });
+
+  describe('validate', () => {
+    it("should return true when the raw password's hash is valid", async () => {
+      const raw = 'xyz';
+      const password = new Password();
+      password.user = user;
+      password.hash = 'abc';
+
+      jest
+        .spyOn(repo, 'findOne')
+        .mockImplementation(async (options) =>
+          options?.where?.['user']?.username === user.username ? password : null
+        );
+      jest
+        .spyOn(argon2, 'verify')
+        .mockImplementation(async (x, y) => x === raw && y === password.hash);
+
+      const actual = await svc.validate(user.username, raw);
+      expect(actual).toEqual(true);
+    });
+
+    it("should return false when the raw password's hash is invalid", async () => {
+      const raw = 'xyz';
+      const password = new Password();
+      password.user = user;
+      password.hash = 'abc';
+
+      jest
+        .spyOn(repo, 'findOne')
+        .mockImplementation(async (options) =>
+          options?.where?.['user']?.username === user.username ? password : null
+        );
+      jest
+        .spyOn(argon2, 'verify')
+        .mockImplementation(async (x, y) => x === raw && y === password.hash);
+
+      const actual = await svc.validate(user.username, raw + '1');
+      expect(actual).toEqual(false);
+    });
+
+    it('should throw NotFoundException if there are no passwords for the given username', async () => {
+      jest.spyOn(repo, 'findOne').mockResolvedValue(null);
+      expect(
+        async () => await svc.validate(user.username, 'xyz')
+      ).rejects.toThrow(NotFoundException);
+    });
+  });
+});
--- src/auth/session/service/password.service.ts
+++ src/auth/session/service/password.service.ts
@@ -3,39 +3,68 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Password } from '../entity/password.entity';
 import { User } from 'src/auth/user/entity/user.entity';
-import { randomUUID } from 'crypto';
 import argon2 from 'argon2';
 
+/**
+ * Provides services for creating and verifying passwords.
+ */
 @Injectable()
 export class PasswordService {
   constructor(
     @InjectRepository(Password) private readonly REPO: Repository<Password>
   ) {}
 
+  /**
+   * Creates a new password for the given user. The given raw password
+   * will be passed through a secure hashing algorithm so as to not store
+   * it in plain text.
+   *
+   * @param user The user owning the password.
+   * @param rawPassword A raw (plain-text) password.
+   * @returns True when the password is created.
+   */
   async create(user: User, rawPassword: string): Promise<boolean> {
     const password = new Password();
-    password.createUser = user;
-    password.updateUser = user;
-    password.salt = randomUUID();
-    password.hash = await argon2.hash(rawPassword + password.salt);
+    password.user = user;
+
+    // See https://github.com/ranisalt/node-argon2/wiki/Options.
+    password.hash = await argon2.hash(rawPassword);
 
     await this.REPO.save(password);
     return true;
   }
 
-  async update(userId: string, updates: Partial<Password>): Promise<number> {
+  /**
+   * Updates the identified user's password. Similar to {@link create},
+   * the raw password will be hashed before storing in the database.
+   *
+   * @param userId A user id.
+   * @param rawPassword A raw (plain-text) password.
+   * @returns The number of passwords affected by the update (1 or 0).
+   */
+  async update(userId: string, rawPassword: string): Promise<number> {
     const { affected } = await this.REPO.update(
-      { createUser: { id: userId } },
-      updates
+      { userId },
+      { hash: await argon2.hash(rawPassword) }
     );
     return affected ?? 0;
   }
 
+  /**
+   * Verifies that the given raw password's hash matches the one stored for
+   * the given username.
+   *
+   * @param username The username of the owner of the password.
+   * @param rawPassword A raw (plain-text) password.
+   * @returns True if the hashed raw password matches the database record,
+   * or false otherwise.
+   * @throws NotFoundException if a password for the given username does not exist.
+   */
   async validate(username: string, rawPassword: string): Promise<boolean> {
     const password = await this.REPO.findOne({
-      where: { createUser: { username } }
+      where: { user: { username } }
     });
     if (!password) throw new NotFoundException();
-    return await argon2.verify(rawPassword + password.salt, password.hash);
+    return await argon2.verify(rawPassword, password.hash);
   }
 }
