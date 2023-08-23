1417b7c7884da7e75900ae69c1b5e3542b935d43
prado <pradodje@gmail.com>
2023-08-22 23:35:43 -0700

    Add docs

--- src/auth/session/service/auth.service.ts
+++ src/auth/session/service/auth.service.ts
@@ -11,6 +11,9 @@ import { PasswordService } from './password.service';
 import { SessionService } from './session.service';
 import { Session } from '../entity/session.entity';
 
+/**
+ * Provides authentication services.
+ */
 @Injectable()
 export class AuthService {
   constructor(
@@ -19,6 +22,15 @@ export class AuthService {
     private readonly SES_SVC: SessionService
   ) {}
 
+  /**
+   * Registers a new user with the application. if successful, a new
+   * session is created for the user.
+   *
+   * @param dto A signup DTO.
+   * @returns A session.
+   * @throws BadRequestException if the provided username already exists
+   * in the database.
+   */
   async signup(dto: SignupDto): Promise<Session> {
     const { username, email, password } = dto;
 
@@ -37,6 +49,15 @@ export class AuthService {
     return await this.SES_SVC.create(user);
   }
 
+  /**
+   * Validates the given user credentials. If valid, a new session is created
+   * for the user.
+   *
+   * @param dto A login DTO.
+   * @returns A session.
+   * @throws UnauthorizedException if the password does not match the database
+   * record, or if the username does not exist.
+   */
   async login(dto: LoginDto): Promise<Session> {
     const handleUnauthorized = () => {
       throw new UnauthorizedException('The user credentials are invalid.');
@@ -55,6 +76,12 @@ export class AuthService {
     return await this.SES_SVC.create(user);
   }
 
+  /**
+   * Invalidates the session for the identified user.
+   *
+   * @param id A user id.
+   * @returns True if the logout was successful, or false otherwise.
+   */
   async logout(id: string): Promise<boolean> {
     return (await this.SES_SVC.delete({ createUser: { id } })) > 0;
   }
